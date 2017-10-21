2bd7fa764c737e5b44881732cb2c4d620147a54c
Unknown <x.zenteno.a@gmail.com>
2017-10-21 13:55:56 -0300

    Se agrega websocket server

---
+++ ws.py
@@ -0,0 +1,22 @@
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+usuarios = []
+
+class SimpleEcho(WebSocket):
+
+    def handleMessage(self):
+    	print self.data
+    	for u in usuarios:
+			if u!=self:
+				u.sendMessage(self.data)
+
+    def handleConnected(self):
+    	print(self.address, 'connected')
+    	usuarios.append(self)
+
+    def handleClose(self):
+    	usuarios.remove(self)
+        print(self.address, 'closed')
+
+server = SimpleWebSocketServer('127.0.0.1', 8000, SimpleEcho)
+server.serveforever()
